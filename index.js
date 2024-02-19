d08dc2bafbc8ff7c61909f07d2b43f8bf223871f
Alain <alain.devcs@gmail.com>
2024-02-19 06:44:17 +0100

    html gen

--- index.js
+++ index.js
@@ -1,3 +1,78 @@
 // Import xml reader
-const xmlReader = require('xml-reader');
+const { XMLParser } = require("fast-xml-parser");
+const fs = require("fs");
+const { join } = require("path");
 
+function getJsonThreads() {
+  // Read the XML file
+  const input = fs.readFileSync(join("input", "input.xml"), "utf8");
+
+  const set = new Set();
+
+  const threads = {};
+
+  // Read the XML file
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+  });
+  let json = parser.parse(input);
+
+  for (const sms of json.smses.sms) {
+    if (!threads[sms["@_address"]]) {
+      threads[sms["@_address"]] = {
+        from: sms["@_contact_name"],
+        messages: [],
+      };
+    }
+
+    // Generate unique hash
+    const hash = `${sms["@_address"]}-${sms["@_body"]}`;
+    if (set.has(hash)) {
+      continue;
+    }
+    set.add(hash);
+
+    threads[sms["@_address"]].messages.push({
+      date: sms["@_readable_date"],
+      body: sms["@_body"],
+    });
+  }
+
+  // console.log(JSON.stringify(threads, null, 2));
+  return threads;
+}
+
+function generateWebpage(threads) {
+  // Create HTML content
+  let htmlContent =
+    "<!DOCTYPE html>\n<html>\n<head>\n<title>Message Threads</title>\n</head>\n<body>\n<h1>Message Threads</h1>\n<ul>\n";
+
+  // Loop through phone numbers
+  Object.keys(threads).forEach((phoneNumber) => {
+    const contactName = threads[phoneNumber].from;
+    const messages = threads[phoneNumber].messages;
+
+    // Create list item for each phone number
+    htmlContent += `<li><h3>${contactName} - ${phoneNumber}</h3>\n<ul>\n`;
+
+    // Add messages for each phone number
+    messages.forEach((message) => {
+      htmlContent += `<li>${message.date} - ${message.body}</li>\n`;
+    });
+
+    htmlContent += "</ul>\n</li>\n";
+  });
+
+  // Close HTML content
+  htmlContent += "</ul>\n</body>\n</html>";
+
+  // Write HTML content to file
+  fs.writeFileSync(join("output", "index.html"), htmlContent, "utf-8");
+}
+
+function main() {
+  const threads = getJsonThreads();
+  generateWebpage(threads);
+}
+
+main();
